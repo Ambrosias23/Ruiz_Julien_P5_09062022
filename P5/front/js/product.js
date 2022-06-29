401a1332b40c9cc1c447e1106973b84a007460fb
Julien Ruiz <ambrosias23@gmail.com>
2022-06-29 12:31:52 +0200

    Milieu de projet

--- P5/front/js/product.js
+++ P5/front/js/product.js
@@ -1,31 +1,77 @@
 let url = new URL(window.location.href);
-    let id = url.searchParams.get('id');
-    console.log(id);
+let productId = url.searchParams.get('id');
+//console.log(productId);
 
-fetch("http://localhost:3000/api/products/"+id)
+fetch("http://localhost:3000/api/products/"+productId)
 .then(function(res) {
   if (res.ok) {
     return res.json();
   }
 })
 .then(function(value) {
-let products = value;
+	let product = value;
+	//console.log(product);
 
-console.log(products);
+	// Assigning img data
+	let imgContainer = document.querySelector(".item__img");
+	let img = document.createElement("img");
+	img.src = product.imageUrl;
+	img.alt = product.altTxt;
+	imgContainer.appendChild(img);
+
+	// Assigning title data
+	title.innerText = product.name;
+
+	// Assigning price data
+	let price = document.getElementById('price');
+	price.innerText = product.price;
+
+	// Assigning description data
+	let description = document.getElementById('description');
+	description.innerText = product.description;
+
+	// Handling select options
+  console.log(product.colors);
+  for(const color of product.colors) {
+    let selectColors = document.getElementById("colors");
+    let option = new Option(color, color);
+    selectColors.appendChild(option);
+  }
+})
+.catch(function(err) {
+	console.log('Une erreur est survenue');
+	console.log(err);
+});
+
+// Listening 'click' on cart button
+const button = document.getElementById('addToCart');
+button.addEventListener('click', function() {
+	button.innerText = "C'est validé !";
+	console.log('clicked');
+});
+
+// Local storage
+
+
+const cart = JSON.parse(localStorage.getItem("cart"));
+
+    button.onclick = () => {
+        
+		const item = {
+            productId : productId,
+            color : colors.value,
+            quantity : quantity.value,
+		}
+        
+		
+		let cart = [];
+		cart.push(item);
+		
+
+	;
+	
+    localStorage.setItem("cart",JSON.stringify(cart))
     
-let img = document.getElementsByClassName("item__img");
-let price = document.getElementById('price');
-let description = document.getElementById('description');
+    };
 
-img.src=products.imageUrl;
-img.alt=products.altTxt;
-title.innerText = products.name;
-price.innerText = products.price;
-description.innerText = products.description;
 
-});
-const button = document.getElementsById('addToCard');    
-button.addEventListener('click', function() {          
-  
-  button.innerText("C'est validé !");            
-});
--- P5/front/js/script.js
+++ P5/front/js/script.js
@@ -1,38 +1,45 @@
 fetch('http://localhost:3000/api/products')
 .then(function(res) {
-  if (res.ok) {
+  if(res.ok) {
     return res.json();
   }
 })
 .then(function(value) {
-  let products = value;
-for(const product of products) {
-  console.log(product);
-  
-  let a= document.createElement("a");
-  let article = document.createElement("article");
-  let img= document.createElement("img");
-  let h3 = document.createElement ("h3");
-  h3.classList.add("productName");
-  let p = document.createElement ("p");
-  p.classList.add("productDescription");
-document.getElementById('items').appendChild(a);
-a.appendChild(article) 
-article.appendChild(img);
-article.appendChild(h3); 
-article.appendChild(p);
-let id=product._id;
-a.href= "./product.html?id="+id;
-img.src= product.imageUrl;
-img.alt=product.altTxt;
-h3.innerText = product.name;
-p.innerText= product.description;
-
-
-
-}})
-.catch(function(err) {
-  console.log ('Une erreur est survenue')
-  console.log (err)
+	let products = value;
+	for(const product of products) {
+		//console.log(product);
+ 
+		// Creating <a>
+		let a = document.createElement("a");
+		a.href = "./product.html?id="+product._id;
+ 
+		// Creating <article>
+		let article = document.createElement("article");
+		a.appendChild(article)
+ 
+		// Creating <img>
+		let img = document.createElement("img");
+		img.src = product.imageUrl;
+		img.alt = product.altTxt;
+		article.appendChild(img);
+ 
+		// Creating <h3>
+		let h3 = document.createElement ("h3");
+		h3.innerText = product.name;
+		h3.classList.add("productName");
+		article.appendChild(h3);
+ 
+		// Creating <p>
+		let p = document.createElement ("p");
+		p.classList.add("productDescription");
+		p.innerText = product.description;
+		article.appendChild(p);
+ 
+		// Appending tag to existing html
+		document.getElementById('items').appendChild(a);
+	}
 })
-;
+.catch(function(err) {
+	console.log('Une erreur est survenue');
+	console.log(err);
+});
