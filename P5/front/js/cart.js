1db0e778474947a3b344a5a1e7769bee1f6f3493
Julien Ruiz <ambrosias23@gmail.com>
2022-07-01 17:04:23 +0200

    Milieu de projet

--- P5/front/js/cart.js
+++ P5/front/js/cart.js
@@ -1,81 +1,96 @@
-let productId = localStorage.productId;
-console.log(productId);
+// Getting back my cart from localstorage
+let cart = []; // Initialisation au cas ou il n'existe pas dans le LS
+let cartFromLS = localStorage.getItem("cart");
+if (cartFromLS !== null) {
+	cart = JSON.parse(cartFromLS);
+}
+//console.log(cart);
+//console.log(cart[0]);
+//console.log(cart[0].productId);
 
-fetch("http://localhost:3000/api/products/"+localStorage.getItem("productId"))
-.then(function(res) {
-  if(res.ok) {
-    return res.json();
-  }
-})
+// Looping on each cartItem from cart
+for(key in cart) {
+	let cartItem = cart[key];
+	fetch("http://localhost:3000/api/products/"+cartItem.productId)
+	.then(function(res) {
+		if(res.ok) {
+			return res.json();
+		}
+	})
+	.then(function(value) {
+		let product = value;
 
-.then(function(value) {
-	let product = value; 
+		// Creating <article>
+		let article = document.createElement("article");
+		article.classList.add("cart__item");
+		article.setAttribute('data-id', cartItem.productId);
+		article.setAttribute('data-color', cartItem.color);
+		cart__items.appendChild(article);
 
-// Creating <article>
-    let article = document.createElement("article");
-    article.classList.add("cart__item");
-//article.dataset.add("{product-ID}");
-//article.dataset.add("{product-color}");
-    cart__items.appendChild(article);
-
-//Creating <div> et <img>
-    let divImg = document.createElement("div");
-    divImg.classList.add("cart__item__img");
-    let img = document.createElement("img");
+		// Creating <div> et <img>
+		let divImg = document.createElement("div");
+		divImg.classList.add("cart__item__img");
+		let img = document.createElement("img");
 		img.src = product.imageUrl;
 		img.alt = product.altTxt;
-    article.appendChild(divImg);
-    divImg.appendChild(img);
+		article.appendChild(divImg);
+		divImg.appendChild(img);
 
-//Creating divContent
-    let divContent = document.createElement("div");
-    divContent.classList.add("cart__item__content");
-    article.appendChild(divContent);
+		// Creating divContent
+		let divContent = document.createElement("div");
+		divContent.classList.add("cart__item__content");
+		article.appendChild(divContent);
 
-//Creating divDescription
-    let divDescription = document.createElement("div");
-    divDescription.classList.add("cart__item__content__description");
-    divContent.appendChild(divDescription);
+		// Creating divDescription
+		let divDescription = document.createElement("div");
+		divDescription.classList.add("cart__item__content__description");
+		divContent.appendChild(divDescription);
 
-//Creating in divDescription of h2 and p 
-    let h2 = document.createElement("h2");
-    h2.innerText = product.name;
-    let colorProduct = document.createElement("p");
-    colorProduct.innerText = localStorage.getItem("color");
-    let priceProduct = document.createElement("p");
-    priceProduct.innerText = product.price + "€";
-    divDescription.appendChild(h2);
-    divDescription.appendChild(colorProduct);
-    divDescription.appendChild(priceProduct);
+		// Creating in divDescription of h2 and p
+		let h2 = document.createElement("h2");
+		h2.innerText = product.name;
+		let colorProduct = document.createElement("p");
+		colorProduct.innerText = cartItem.color;
+		let priceProduct = document.createElement("p");
+		priceProduct.innerText = product.price + "€";
+		divDescription.appendChild(h2);
+		divDescription.appendChild(colorProduct);
+		divDescription.appendChild(priceProduct);
 
-//creating divSetting
-    let divSetting = document.createElement("div");
-    divSetting.classList.add("cart__item__content__settings");
-    divContent.appendChild(divSetting);
+		// Creating divSetting
+		let divSetting = document.createElement("div");
+		divSetting.classList.add("cart__item__content__settings");
+		divContent.appendChild(divSetting);
 
-// creating in divSetting of quantity
-    let divQuantity = document.createElement("div");
-    divQuantity.classList.add("cart__item__content__settings__quantity");
-    divSetting.appendChild(divQuantity);
+		// Creating in divSetting of quantity
+		let divQuantity = document.createElement("div");
+		divQuantity.classList.add("cart__item__content__settings__quantity");
+		divSetting.appendChild(divQuantity);
 
-// Creating in quantity of <p> and <input>
-    let pQte = document.createElement("p");
-    let input = document.createElement("input");
-    input.type = Number;
-    input.classList.add("itemQuantity");
-    input.name = "itemQuantity";
-    input.min = 1;
-    input.max = 100;
-    input.value = 42;
-    divQuantity.appendChild(pQte);
-    divQuantity.appendChild(input);
+		// Creating in quantity of <p> and <input>
+		let pQte = document.createElement("p");
+		let input = document.createElement("input");
+		input.type = Number;
+		input.classList.add("itemQuantity");
+		input.name = "itemQuantity";
+		input.min = 1;
+		input.max = 100;
+		input.value = cartItem.quantity;
+		divQuantity.appendChild(pQte);
+		divQuantity.appendChild(input);
 
-// Creating divDelete and p
-    let divDelete = document.createElement("div");
-    divDelete.classList.add("cart__item__content__settings__delete");
-    let p = document.createElement("p");
-    divDelete.classList.add("deleteItem");
-    p.innerText = "Supprimer";
-    divSetting.appendChild(divDelete);
-    divDelete.appendChild(p);
-    })
+		// Creating divDelete and p
+		let divDelete = document.createElement("div");
+		divDelete.classList.add("cart__item__content__settings__delete");
+		let p = document.createElement("p");
+		p.classList.add("deleteItem");
+		p.innerText = "Supprimer";
+		divSetting.appendChild(divDelete);
+		divDelete.appendChild(p);
+	})
+  
+	.catch(function(err) {
+		console.log('Une erreur est survenue');
+		console.log(err);
+	});
+}
--- P5/front/js/product.js
+++ P5/front/js/product.js
@@ -4,9 +4,9 @@ let productId = url.searchParams.get('id');
 
 fetch("http://localhost:3000/api/products/"+productId)
 .then(function(res) {
-  if (res.ok) {
-    return res.json();
-  }
+	if (res.ok) {
+		return res.json();
+	}
 })
 .then(function(value) {
 	let product = value;
@@ -31,46 +31,48 @@ fetch("http://localhost:3000/api/products/"+productId)
 	description.innerText = product.description;
 
 	// Handling select options
-  console.log(product.colors);
-  for(const color of product.colors) {
-    let selectColors = document.getElementById("colors");
-    let option = new Option(color, color);
-    selectColors.appendChild(option);
-  }
+	console.log(product.colors);
+	for(const color of product.colors) {
+		let selectColors = document.getElementById("colors");
+		let option = new Option(color, color);
+		selectColors.appendChild(option);
+	}
 })
 .catch(function(err) {
 	console.log('Une erreur est survenue');
 	console.log(err);
 });
 
-// Listening 'click' on cart button
-const button = document.getElementById('addToCart');
-button.addEventListener('click', function() {
-	button.innerText = "C'est validé !";
-	console.log('clicked');
-});
-
-// Local storage
-
-
 // Listening to "click" event on cart button
+const button = document.getElementById('addToCart');
 button.addEventListener('click',(event)=>{
-// Creating itemCart
+	
+	
+	// Creating itemCart
 	const cartItem = {
 		productId: productId,
 		color: colors.value,
 		quantity: quantity.value,
 	}
+	
 
-//Getting cart from localstorage
+	// Getting cart from localstorage
 	let cart = []; // Initialisation au cas ou il n'existe pas dans le LS
 	let cartFromLS = localStorage.getItem("cart");
-	if (cartFromLS !== null) {
-		cart = JSON.parse (cartFromLS);
+	if(cartFromLS !== null) {
+		cart = JSON.parse(cartFromLS);
 	}
-
-// Adding the cartItem to the cart
-	cart.push(cartItem);
-// Saving the dart to LS
-	localStorage.setItem("cart",JSON.stringify(cart));
+	if (quantity.value <= 0) {
+        console.log ("Il manque des informations");
+	};
+	
+	if(color = value="--SVP, choisissez une couleur --" ){
+		console.log ("mqe color");
+	};
+	// Adding the cartItem to the cart
+		cart.push(cartItem);
+	// Saving the cart to LS
+		localStorage.setItem("cart",JSON.stringify(cart));
+	
+	
 })
